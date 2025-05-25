1907f3bfb1ff2420104b588573e71f776f5f2076
Akon-911 <Mananjerath9582@gmail.com>
2025-05-26 00:38:00 +0530

    initial prototype (UI needs some work)

---
+++ script.js
@@ -0,0 +1,15 @@
+const winningcondition = {
+    "123":true,
+    "456":true,
+    "789":true,
+    "159":true,
+    "147":true,
+    "258":true,
+    "356":true,
+    "369":true
+}
+const board = new Array(9);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const status = document.getElementById('status');
+});
