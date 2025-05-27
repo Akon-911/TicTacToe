e5fd7ae70b0376c11e8194dee02a29c5a94daf7b
Akon-911 <Mananjerath9582@gmail.com>
2025-05-28 00:20:07 +0530

    UI Update

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const winningcondition = {
-    "123":true,
-    "456":true,
-    "789":true,
-    "159":true,
-    "147":true,
-    "258":true,
-    "356":true,
-    "369":true
-}
+const winningcondition = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[1,4,7],[2,5,8],[3,5,6],[3,6,9]];
 const board = new Array(9);
+const XArray = [];
+const OArray = [];
 let currentTurn = "X";
-let currentMode;
+let currentMode = "H";
+
+
+function toCheck(checker) {
+    console.log("click")
+    if (checker.disabled === true) return;
+    else if (checker.checked === true) {
+        currentMode = 'C';
+        console.log(1);
+    } else {
+        currentMode = 'H';
+        console.log(2)
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    
+    console.log("switcher add on process")
+
     const status = document.getElementById('status');
 });
+    
+
